15e90142e9016a6ca6c221d0b3179aa87d4b60c5
firdavs <frontendhtml1234@gmail.com>
2023-08-16 18:07:18 +0500

    first commit

---
+++ meain.js
@@ -0,0 +1,122 @@
+// const adress = prompt('api adressnini kirinting')
+
+// function salom(adress) {
+//    adress = adress.split(".")
+//     console.log(adress);
+//     return adress
+// }
+
+
+// salom(adress)
+
+// console.log(13 && 11);
+
+
+
+// for (let index = 0; index <= 100 ;   index++   ) {
+//  if (index % 2 == 0 ) {
+//     console.log(index);
+//  }
+  
+// }
+
+// const numbers =[1,2,3,4,5]
+
+// //  const newnambers = numbers.map( (salom) => salom ** 2 )
+ 
+// // console.log(newnambers);
+// const new1 = []
+
+// numbers.forEach ((salom) =>  {
+//    new1.push(salom ** 2)
+//    })
+ 
+
+ 
+
+// console.log(new1);
+
+
+
+
+// const movei = [
+// {name : 'Firdavs', id:1, rating : 9},
+// {name : 'Aboss', id:2, rating : 8},
+// {name : 'ALISHER', id:3, rating : 7},
+// {name : 'Eshak', id:4, rating : 6}
+// ]
+
+
+// const m = movei.map((moves)=>{
+//     // return  {name : moves.name  , id : moves.id, rating: moves.rating + 1 }
+//   if (moves.id == 3) {
+//     return moves.name = 'salom abos'
+// }else{
+    
+// }
+
+// })
+// console.log(movei);
+
+
+// let a = prompt('nima kerak')
+
+
+// function nono() {
+//   if(a == 'somsa' ){
+//   var son = prompt('nechta')
+
+//   if (son) {
+//     alert('mana senga' + ' ' + son)
+//   } else{
+//     alert('qolmagan')    
+//   }
+//   }else{ 
+//     alert('bir gap aytaymi tur yuqol')
+//   } 
+// }
+
+// nono()
+ 
+
+
+
+const malumot = [
+    {
+        name:'osh'
+    },
+    {
+        name:'somsa'
+    },
+    {
+      name:'lavash'
+    }
+]
+let a = prompt('nima kerak')
+malumot.forEach((el)=>{
+  console.log(el);
+ 
+  if(el.name == a ){
+alert('bor')
+  }
+}
+
+  
+)
+
+
+
+
+//  function meme() {
+
+// let nozim = prompt('nima kerak')
+//     if (nozim == malumot[0,1].name) {
+//         alert('salom')
+//     }else{
+//         alert('error')
+//     }
+//  }
+
+//  meme()
+
+ 
